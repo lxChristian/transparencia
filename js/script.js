85e408a685d923f2d64872e72fb24f08a3c7b95c
lxChristian <alxchrstn61@gmail.com>
2024-04-30 14:13:26 -0300

    commit 2

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// Acessar Pagina
+
 const linksPaginas = document.querySelectorAll("[data-pagina]");
 
 linksPaginas.forEach((link) => {
@@ -13,4 +15,36 @@ function avancaPagina(link) {
     .then((body) => {
       conteudo.innerHTML = body;
     });
+}
+
+// Acessibilidade
+
+const btnsAcessibilidade = document.querySelectorAll("[data-acessibilidade]");
+
+btnsAcessibilidade.forEach((btn) => {
+  btn.addEventListener("click", acessibilidade);
+});
+
+let fonte = 16;
+
+function acessibilidade() {
+  const html = document.documentElement;
+  switch (this.dataset.acessibilidade) {
+    case "aumentaFonte":
+      if (html.style.fontSize < "22px") {
+        html.style.fontSize = `${++fonte}px`;
+      }
+      break;
+
+    case "diminuiFonte":
+      if (html.style.fontSize > "12px") {
+        html.style.fontSize = `${--fonte}px`;
+      }
+      break;
+
+    case "contraste":
+      this.classList.toggle("ativo");
+      html.classList.toggle("contraste");
+      break;
+  }
 }
