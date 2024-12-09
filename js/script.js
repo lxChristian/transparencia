afc0063f06cfe36542f20f99fa30f24c5f400a05
lxChristian <alxchrstn61@gmail.com>
2024-12-09 15:45:38 -0300

    update

--- js/script.js
+++ js/script.js
@@ -2,19 +2,21 @@
 
 const linksPaginas = document.querySelectorAll("[data-pagina]");
 
-linksPaginas.forEach((link) => {
-  link.addEventListener("click", avancaPagina);
-});
+if (linksPaginas.length) {
+  linksPaginas.forEach((link) => {
+    link.addEventListener("click", avancaPagina);
+  });
 
-function avancaPagina(link) {
-  link.preventDefault();
-  const conteudo = document.querySelector("[data-conteudo]");
-  const pagina = link.srcElement.getAttribute("href");
-  fetch(pagina)
-    .then((response) => response.text())
-    .then((body) => {
-      conteudo.innerHTML = body;
-    });
+  function avancaPagina(link) {
+    link.preventDefault();
+    const conteudo = document.querySelector("[data-conteudo]");
+    const pagina = link.srcElement.getAttribute("href");
+    fetch(pagina)
+      .then((response) => response.text())
+      .then((body) => {
+        conteudo.innerHTML = body;
+      });
+  }
 }
 
 // Acessibilidade
